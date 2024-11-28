ccb4762f427a903260ebdaf2cd2ed6de686f54eb
Mia <atasdew@mail.ru>
2024-11-28 22:11:56 +0300

    H/W

---
+++ src/test/java/AddRemoveElementsTest.java
@@ -0,0 +1,49 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import java.time.Duration;
+import java.util.List;
+
+public class AddRemoveElementsTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup () {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+//        options.addArguments("headless");
+//        options.addArguments("incognito");
+//        options.addArguments("disable-notification");
+        driver= new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
+    }
+
+    @Test
+    public void checkAddRemoveElements () {
+        // Добавление двух элементов
+        for (int i = 0; i < 2; i++) {
+            driver.findElement(By.xpath("//*[@id=\"content\"]/div/button")).click();
+        }
+        List<WebElement> buttons2 = driver.findElements(By.className("added-manually"));
+        Assert.assertEquals(2, buttons2.size());
+
+        // Удаление одного элемента
+        driver.findElement(By.xpath("//*[@id=\"elements\"]/button")).click();
+        List<WebElement> button1 = driver.findElements(By.className("added-manually"));
+        Assert.assertEquals(1, button1.size());
+    }
+
+    @AfterMethod (alwaysRun = true)
+    public void tearDown () {
+        driver.quit();
+    }
+
+}
+ 
---
+++ src/test/java/CheckboxesTest.java
@@ -0,0 +1,49 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class CheckboxesTest {
+    WebDriver driver;
+    @BeforeMethod
+    public void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
+
+    }
+    @Test
+    public void checkCheckboxes() {
+        //Чек-бокс первый
+        WebElement checkBoxElement1 = driver.findElement(By.xpath("//*[@id=\"checkboxes\"]/input[1]"));
+        // Никак не могу его найти через cssSelector, поэтому пользуюсь xpath.
+        // Пример моей попытки: driver.findElement(By.cssSelector("input[wfd-id='id0']"));
+        boolean isSelectedBeforeClick1 = checkBoxElement1.isSelected();
+        Assert.assertFalse(isSelectedBeforeClick1);
+        checkBoxElement1.click();
+        boolean isSelectedAfterClick1 = checkBoxElement1.isSelected();
+        Assert.assertTrue(isSelectedAfterClick1);
+
+        //Чек-бокс второй
+        WebElement checkBoxElement2 = driver.findElement(By.xpath("//*[@id=\"checkboxes\"]/input[2]"));
+        boolean isSelectedBeforeClick2 = checkBoxElement2.isSelected();
+        Assert.assertTrue(isSelectedBeforeClick2);
+        checkBoxElement2.click();
+        boolean isSelectedAfterClick2 = checkBoxElement2.isSelected();
+        Assert.assertFalse(isSelectedAfterClick2);
+        }
+    @AfterMethod(alwaysRun = true)
+    public void tearDown () {
+        driver.quit();
+    }
+}
+
